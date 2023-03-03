aa4681b1c616ebfe4895860deba47a3dd6c6f31e
RoderCode <Rodersnow@gmail.com>
2023-03-03 12:44:59 +0100

    create post method createGuildWalk

--- src/main/java/com/example/groupwalkapi/api/GuildWalkRestController.java
+++ src/main/java/com/example/groupwalkapi/api/GuildWalkRestController.java
@@ -1,14 +1,26 @@
 package com.example.groupwalkapi.api;
 
+import com.example.groupwalkapi.bean.GuildWalk;
 import com.example.groupwalkapi.service.GuildWalkService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("guild-walks")
 public class GuildWalkRestController {
-    private final GuildWalkService groupWalkService;
+    private final GuildWalkService guildWalkService;
     @Autowired
-    public GuildWalkRestController(GuildWalkService groupWalkService) {
-        this.groupWalkService = groupWalkService;
+    public GuildWalkRestController(GuildWalkService guildWalkService) {
+        this.guildWalkService = guildWalkService;
+    }
+    @PostMapping
+    private ResponseEntity<GuildWalk> createGuildWalk(@RequestBody GuildWalk guildWalk){
+        guildWalkService.createGuildWalk(guildWalk);
+        return new ResponseEntity<>(guildWalk, HttpStatus.CREATED);
     }
 }
