ff94f568e8fbd6db6b143e5039ddda58ceefe44c
RoderCode <Rodersnow@gmail.com>
2023-03-03 11:30:07 +0100

    create ListEmptyException

--- src/main/java/com/example/groupwalkapi/handler/GuildWalkRestExceptionHandler.java
+++ src/main/java/com/example/groupwalkapi/handler/GuildWalkRestExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.example.groupwalkapi.exception;
+package com.example.groupwalkapi.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
