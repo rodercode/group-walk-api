6cfffb3b321b14bf9bf343e3167bebab88361d7d
RoderCode <Rodersnow@gmail.com>
2023-03-03 10:27:17 +0100

    first commit

---
+++ src/test/java/com/example/groupwalkapi/GroupWalkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groupwalkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupWalkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
