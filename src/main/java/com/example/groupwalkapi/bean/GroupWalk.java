dc1562667b5441cf026a468bec302dd0fb357889
RoderCode <Rodersnow@gmail.com>
2023-03-03 11:14:53 +0100

    update GroupWalk Entity

--- src/main/java/com/example/groupwalkapi/bean/GroupWalk.java
+++ src/main/java/com/example/groupwalkapi/bean/GroupWalk.java
@@ -1,7 +1,19 @@
 package com.example.groupwalkapi.bean;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.apache.hc.client5.http.RouteInfo;
 
-@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "group_walks")
 public class GroupWalk {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private int members;
+    private RouteInfo routeInfo;
 }
